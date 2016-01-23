91cd1b260ae10b29ab27ec2c8cd533068daa4ee3
Umut Afacan <umut-afacan@hotmail.com>
2016-01-23 23:17:39 +0200

    Initial Commit

---
+++ Instalker/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Instalker
+//
+//  Created by umut on 1/23/16.
+//  Copyright © 2016 SmartClick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Instalker/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Instalker
+//
+//  Created by umut on 1/23/16.
+//  Copyright © 2016 SmartClick. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
